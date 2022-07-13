38e31f4e59e8582789c7d343343fc3820c851a26
TessBReid <101984008+TessBReid@users.noreply.github.com>
2022-07-13 17:22:51 -0400

    Updating file to make sure connection is correct.

--- src/Weather.js
+++ src/Weather.js
@@ -4,6 +4,7 @@ export default function Weather() {
   return (
     <div>
       <h1>Hello From Weather</h1>
+      <p>This is my weather app that I have made with SheCodes.</p>
     </div>
   );
 }
